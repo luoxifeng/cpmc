4b116283ddcd67ead8cda60cbb53066f6916f603
luoxifeng <942394682@qq.com>
2020-04-20 17:13:31 +0800

    add ts

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,40 @@
 
 import { TYPES } from './types';
 
-export function createProcessManagerCreator(
-  { type = TYPES.ALL } = { type: TYPES.ALL }
-) {
+type j = any[];
+
+export function createProcessManagerCreator({
+  type = TYPES.ALL
+} = { type: TYPES.ALL }) {
   let baseCount = 0,
     usedCount = 0,
     baseResolve = () => { };
   const allProcess: Promise<any>[] = [];
-  const basePromise = new Promise(res => baseResolve = res);
-  const resolveWrapper = resolve => arg => {
+  const basePromise = new Promise(res => baseResolve = () => res(allProcess));
+  const resolveWrapper = <T>(resolve: (res: T) => void) => (arg: T) => {
     (type === TYPES.RACE || (baseCount === ++usedCount)) && baseResolve();
     resolve(arg);
   };
-  const createProcess = () => {
-    let resolve = () => {};
-    let reject = () => {};
-    const promise = new Promise((...cbs) => [resolve, reject] = cbs);
+
+  const createProcess = <T>() => {
+    let resolve = (res: T) => console.log(res);
+    let reject = (res: any) => console.error(res);
+    const promise = new Promise<T>((...cbs) => [resolve, reject] = cbs);
     allProcess.push(promise);
     baseCount++;
+
     return {
       resolve: resolveWrapper(resolve),
-      reject: arg => {
+      reject: (arg: any) => {
         baseResolve();
         reject(arg);
       }
     };
   };
-  const createManager = (...args) => basePromise
-    .then(() => Promise[type](allProcess)).then(...args);
+
+  const createManager = (...args: any[]) => basePromise
+    .then(Promise[type].bind<any>(Promise))
+    .then(...args);
 
   return [createProcess, createManager];
 }
